0ddfdeb76a6c74ded10db1325e7bc09d5a40a47f
NickChapman1988 <muppetnj_uk@hotmail.com>
2021-05-22 11:55:59 +0000

    Added sendMail function to sendEmail.js file

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,17 @@
-console.log("Hello")
+/*Code build following walkthrough from Code Institute 'Resume' project*/
+
+function sendMail(contactForm) {
+    emailjs.send("outlook","feeling_hungry", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "contact_message": contactForm.contactmessage.value
+    })
+    .then(
+        function(response) {
+            console.log("Success", response)
+        }, 
+        function(error) {
+            console.log("Failed", error);
+        });
+    return false;    
+}
