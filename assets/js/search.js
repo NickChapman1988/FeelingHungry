7620e62d56bd95b976b68acbdceab75b88c4fd3b
NickChapman1988 <muppetnj_uk@hotmail.com>
2021-05-26 20:47:50 +0000

    Added functionality to getRecipes function to display content in html element

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,25 +1,36 @@
 var url = "https://api.spoonacular.com/recipes/findByIngredients"
 var apiKey = "apiKey=4046887ae70f4afda1862925dff6697c"
-var recipeSearch = new XMLHttpRequest();
 
 function getRecipes() {
     var ingredients = $("#search-box").val();
+    var recipeSearch = new XMLHttpRequest();
     
-    if (!ingredients) {
-        $("#recipe-box").html(`<h2>We're sorry, we couldn't find any recipes with those ingredients</h2>`);
-        return;
-    }
-
     recipeSearch.open ("GET", url + "?ingredients=" + ingredients + "&" + apiKey, true);
     recipeSearch.send();
 
     recipeSearch.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             console.log(JSON.parse(this.responseText));
-            var recipes = (this.responseText);
-                
-            for (let i = 0; i < recipes.length ; i++) {
-            $("#recipe-box").html(`<h2>${recipes[i].title}</h2>`)
-        }}
-    };
-}
+            var recipes = (JSON.parse(this.responseText));   
+     
+            recipes.forEach(function(item) {
+                newRecipe = new XMLHttpRequest();
+                newRecipe.open ("GET", "https://api.spoonacular.com/recipes/" + item.id + "/information?includeNutrition=false&" + apiKey, true);
+                newRecipe.send();
+
+                newRecipe.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        console.log(JSON.parse(this.responseText));
+                        var recipeInstructions = (JSON.parse(this.responseText));
+                        
+                        document.getElementById("recipe-box").innerHTML += `<h2> ${item.title}</h2> <img src=${item.image}> <p>${item.sourceUrl}</p>`
+                    }  
+                }
+            }); 
+        }
+    }    
+};
+
+
+
+
