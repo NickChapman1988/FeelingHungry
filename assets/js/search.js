703fa6ceefa028d2d06756062ee0d43a789b5779
NickChapman1988 <muppetnj_uk@hotmail.com>
2021-06-05 12:45:17 +0000

    Beautified JS code

--- assets/js/search.js
+++ assets/js/search.js
@@ -3,7 +3,7 @@ var apiKey = "apiKey=4046887ae70f4afda1862925dff6697c";
 
 function fillRandomRecipes() {
     var randomRecipeSearch = new XMLHttpRequest();
-    var urlRandom = "https://api.spoonacular.com/recipes/random?number=9&"
+    var urlRandom = "https://api.spoonacular.com/recipes/random?number=9&";
 
     randomRecipeSearch.open ("GET", urlRandom + apiKey, true);
     randomRecipeSearch.send();
@@ -18,10 +18,9 @@ function fillRandomRecipes() {
             document.getElementById("recipe-box").innerHTML += `<div class="col-md-6 col-lg-4 recipe-card hvr-grow"><div class="card bg-light"><h4 class="card-title">${item.title}</h4>
             <img src=${item.image} class="img-fluid card-image-top"><a href="${item.spoonacularSourceUrl}" target="_blank" class="btn recipe-btn">View Recipe</a></div></div>`;              
             });
-        };
+        }
     };
 }
-
 
 function getRecipes(recipes) {
     var ingredients = $("#search-box").val();
